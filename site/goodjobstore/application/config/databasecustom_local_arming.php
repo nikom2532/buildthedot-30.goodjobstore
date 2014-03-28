c666670d18491aeb5c055577a278b39696d074b4
arming <nikom2532@gmail.com>
2014-03-28 11:40:13 +0700

    Add edit delete Database Configs

---
+++ site/goodjobstore/application/config/databasecustom_local_arming.php
@@ -0,0 +1,5 @@
+<?php
+$objCon = mysql_connect("localhost","iming","iming") or die(mysql_error());
+$objDB = mysql_select_db("buildthedot_30goodjobstore") or die("Can't connect Database");
+mysql_query("SET NAMES utf8",$objCon);
+?>
