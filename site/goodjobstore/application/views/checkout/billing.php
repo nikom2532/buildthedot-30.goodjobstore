0cd770b6cd8b1a423f5d1dde01e07f64a65fe750
arming <nikom2532@gmail.com>
2014-04-09 18:21:24 +0700

    update UPS fluctuation yearly

--- site/goodjobstore/application/views/checkout/billing.php
+++ site/goodjobstore/application/views/checkout/billing.php
@@ -116,7 +116,16 @@
 			}
 		}
 	</script>
-
+	<?php
+		//#########################################
+		/* $fluctuationYearly คือค่าที่เปลี่ยนแปลง หลังจาก ค่าต้นฉพับ ในปี 2012 */
+		// $fluctuationYearly จะเพิ่มขึ้นปีละ 15% ต่อปี
+		// $fluctuationYearly --> 2013 = 1.15 เท่า ของ Rate ปี 2012
+		// $fluctuationYearly --> 2014 = 1.31 เท่า ของ Rate ปี 2012
+		$fluctuationYearly = 1.31;
+		
+		//#########################################
+	?>
 		<!-- Body Section -->
 		<div id="title_head">
 			Checkout
@@ -439,16 +448,7 @@
 									?>
 								<?php endforeach; ?>
 
-								<?
-									//#########################################
-									/* $fluctuationYearly คือค่าที่เปลี่ยนแปลง หลังจาก ค่าต้นฉพับ ในปี 2012 */
-									// $fluctuationYearly จะเพิ่มขึ้นปีละ 15% ต่อปี
-									// $fluctuationYearly --> 2013 = 1.15 เท่า ของ Rate ปี 2012
-									// $fluctuationYearly --> 2014 = 1.31 เท่า ของ Rate ปี 2012
-									$fluctuationYearly = 1.31;
-									
-									//#########################################
-								
+								<?php
 									$sqlUPS = "SELECT ups_type.Type_ID, type_name, price, price_saver
 												FROM ups_rate JOIN ups_service ON ups_rate.Zone_ID=ups_service.Zone_ID
 												JOIN ups_type ON ups_service.Type_ID=ups_type.Type_ID";
@@ -658,7 +658,7 @@
 //	$discountShipping = cal_range_weight($order->How_ID, $TotalWeightDimension)*(90/100) * $FuelSurcharge;
 //else 
 if($order->How_ID==3 OR $order->How_ID==4)
-	$discountShipping = cal_range_weight($order->How_ID, $TotalWeightDimension) * $FuelSurcharge;
+	$discountShipping = cal_range_weight($order->How_ID, $TotalWeightDimension) * $FuelSurcharge * $fluctuationYearly;
 else
 	$discountShipping = cal_range_weight($order->How_ID, $order->Total_Weight);
 $exShipping = number_format($discountShipping, 2);
