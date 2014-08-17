365a8e4d7c1b738de5135c8318f0bb03b28af895
Arming Huang <arming@buildthedot.com>
2014-08-17 14:56:46 +0700

    Update Database Data File (UPS-Rate)

---
+++ Database/goodjob_ups_rate_data4.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `ups_rate` ( `Zone_ID`, `weight_min`, `weight_max`, `price`, `price_saver`) VALUES
+(2, 300, 499, 193.2, 182.4),
+(2, 499, 500, 190.2, 179.4),
+(2, 500, 999, 190.2, 179.4),
+(2, 1000, 1e+07, 189, 178.2);
